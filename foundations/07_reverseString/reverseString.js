34cc32a46d69ddb010c13bb0629b8b843a1d7058
kejoness <kjones9843@gmail.com>
2026-01-27 10:09:53 -0500

    remove test skips and implement array methods

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    words = text.split("");
+    words.reverse();
 
+    return words.join("");
 };
+
+text = "hello there";
+console.log(reverseString(text));
 
 // Do not edit below this line
 module.exports = reverseString;
