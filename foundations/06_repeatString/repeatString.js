ca6e27fe6a92dfe0930e52a9c502796ebab8313a
kejoness <kjones9843@gmail.com>
2026-01-27 09:38:30 -0500

    remove test skips and concatenate strings in loop using + operator

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    repetition = 0;
+    newStr = "";
 
+    if (num < 0) {
+        console.log("ERRROR");
+    }
+
+    while (repetition != num) {
+        if (num >= 0) {
+            newStr = newStr + string;
+            repetition++;
+        } else if (num < 0) {
+            return "ERROR";
+        }
+        
+    }
+
+    return newStr;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
