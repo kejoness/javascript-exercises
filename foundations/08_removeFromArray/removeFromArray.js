6a793f4d41a0a7aed30285ec742caf5e719e68e7
kejoness <kjones9843@gmail.com>
2026-01-28 10:52:45 -0500

    fix arg array not printing values

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(args, ...arrayElements) {
+
+    for(let arg of args) {
+        if (args.includes(arrayElements)) {
+            args.splice(args.indexOf(arrayElements), arrayElements);
+        } else {
+            console.log("That element does not exist!");
+        }
+    }
 };
+
+let numbers = [1, 2, 3, 4];
+removeFromArray(numbers, 1);
 
 // Do not edit below this line
 module.exports = removeFromArray;
