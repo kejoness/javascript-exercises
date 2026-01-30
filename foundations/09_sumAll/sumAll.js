5d206f3146fd043cd1914278d14eaecb6bc33fc8
kejoness <kjones9843@gmail.com>
2026-01-30 11:22:28 -0500

    convert integers passed in sumAll to array

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    //convert int to array
+    digit1 = Array.from(String(number1), Number);
+    digit2 = Array.from(String(number2), Number);
+    digits = digit1.concat(digit2);
 
+    console.log(digits);
 };
+
+sumAll(1, 2);
 
 // Do not edit below this line
 module.exports = sumAll;
