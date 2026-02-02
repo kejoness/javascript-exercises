a9c0f1679212e9e1855545c80a037b4d7020414e
kejoness <kjones9843@gmail.com>
2026-02-02 08:24:06 -0500

    convert int to array of digits

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -4,10 +4,12 @@ const sumAll = function(number1, number2) {
     digit2 = Array.from(String(number2), Number);
     digits = digit1.concat(digit2);
 
-    console.log(digits);
+    // use Gauss (n(n+1)/2) sum to find
+    // sum of all numbers
+    
 };
 
-sumAll(1, 2);
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
